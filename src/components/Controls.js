81cc82a7f7a5a4c51b387498ad0e6a1b18f3333e
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2021-08-05 21:11:35 -0700

    add handleInput to Controls component

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const Controls = ({gridWidth, setGridWidth}) => {
+
+    const handleInput = (event) => {
+        setGridWidth(event.target.value);
+    }
+
     return (
         <div id="controls">
 
             <button id="control_button" onClick={() => setGridWidth(gridWidth - 1)}>Fewer Colums</button>
-            <input type="number" value={gridWidth}></input>
+            <input type="number" value={gridWidth} onInput={handleInput}></input>
             <button id="control_button" onClick={() => setGridWidth(gridWidth + 1)}>More Columns</button>
 
         </div>
